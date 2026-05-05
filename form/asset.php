0e6eda83e9ca6221c252fa330c126dfb953c8ed1
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2026-05-05 16:54:21 +0800

    rename table name and relevant codes. Add timezone. Add record date added.

--- form/asset.php
+++ form/asset.php
@@ -4,19 +4,24 @@ include 'function.php';
 $content = '';
 //TODO: redirect out if not certain role
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if(isset($_POST['bd-addasset']))
+    if(isset($_POST['asset_add']))
     {
-        $handlerid = $_POST['bd-assethandler'];
-        $assetid = $_POST['bd-assetid'];
-        $assetlabel = $_POST['bd-assetlabel'];
-        $assettype = $_POST['bd-assettype'];
+        $timestamp = date('m/d/Y H:i:s', time());
+        $details = json_encode([
+            "added" => $timestamp
+        ],JSON_PRETTY_PRINT);
+        //$details = json_encode($detailsdata,JSON_PRETTY_PRINT);
+        $handlerid = $_POST['T2_handlerid_add'];//* For now be the id of current user(Only allow BD staff)
+        $assetid = $_POST['T2_assetid_add'];
+        $assetlabel = $_POST['T2_label_add'];
+        $assettype = $_POST['T2_type_add'];
         $assetstatus = "good";
-        $stmt = $conn->prepare("INSERT INTO bd_asset (assetid,label,type,handlerid,status) VALUES(?,?,?,?,?)");
-        $stmt->bind_param("sssss",$assetid,$assetlabel,$assettype,$handlerid,$assetstatus);
+        $stmt = $conn->prepare("INSERT INTO T2_asset (T2_assetid,T2_label,T2_type,T2_handlerid,T2_status,T2_details) VALUES(?,?,?,?,?,?)");
+        $stmt->bind_param("ssssss",$assetid,$assetlabel,$assettype,$handlerid,$assetstatus,$details);
         if($stmt->execute())
         {
             header('asset.php');
-            alert($_POST['bd-assetid'] . " added by " . $_POST['bd-assethandler']);
+            alert($_POST['T2_assetid_add'] . " added by " . $_POST['T2_handlerid_add'] );
         }
         $stmt->close();
     }
@@ -30,12 +35,12 @@ if($_GET)
         <form class="w-75 m-auto" action="" method="post">
             <h1>Tambah Aset</h1>
             <input class="form-control" name="userid" type="text" hidden>
-            <input class="form-control" name="bd-assethandler" type="text" value="<?php echo(e($_SESSION['userid']));//set handler be current user?>" hidden>
-            <input class="form-control" type="text" name="bd-assetid" id="" placeholder="ID Aset">
-            <input class="form-control" type="text" name="bd-assetlabel" id="" placeholder="Label">
+            <input class="form-control" name="T2_handlerid_add" type="text" value="<?php echo(e($_SESSION['userid']));//set handler be current user?>" hidden>
+            <input class="form-control" type="text" name="T2_assetid_add" id="" placeholder="ID Aset">
+            <input class="form-control" type="text" name="T2_label_add" id="" placeholder="Label">
             <div class="row">
                 <div class="col-6">
-                    <select class="form-control" name="bd-assettype" id="">
+                    <select class="form-control" name="T2_type_add" id="">
                         <option value="laptop">Laptop</option>
                         <option value="projector">Projektor</option>
                         <option value="monitor">Monitor</option>
@@ -46,7 +51,7 @@ if($_GET)
                     <!-- <input class="form-control" name="bd-assetinventory" type="number" id="" value="0"> -->
                 </div>
                 <div class="col-3">
-                    <input class="form-control" name="bd-addasset" type="submit" value="Tambah">
+                    <input class="form-control" name="asset_add" type="submit" value="Tambah">
                 </div>
                 <div id="details">
                     Details
@@ -67,21 +72,21 @@ if($_GET)
 else{
     ob_start();?>
     <h1>Senarai Aset</h1>
-    <table class="table w-75 m-auto">
+    <table class="table table-bordered w-75 m-auto">
         <tr>
             <th>Label</th>
             <th>Jenis</th>
         </tr>
         <?php
-        $stmt = $conn->prepare("SELECT * FROM bd_asset");
+        $stmt = $conn->prepare("SELECT * FROM T2_asset");
         $stmt->execute();
         $data = $stmt->get_result();
         while($list = mysqli_fetch_assoc($data))
         {
             ?>
             <tr>
-                <td><?php echo($list['label']);?></td>
-                <td><?php echo($list['type']);?></td>
+                <td><?php echo($list['T2_label']);?></td>
+                <td><?php echo($list['T2_type']);?></td>
             </tr>
             <?php
         }
--- form/function.php
+++ form/function.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
-$stmt = $conn->prepare("SELECT * FROM user LIMIT 1");
+$stmt = $conn->prepare("SELECT * FROM T1_user LIMIT 1");
 $stmt->execute();
 $userinfo = $stmt->get_result();
 while($u_r = mysqli_fetch_assoc($userinfo))
 {
-    $_SESSION['userid'] = $u_r['userid'];
+    $_SESSION['userid'] = $u_r['T1_userid'];
 }
 $stmt->close();
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
 
 function e($string) {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
