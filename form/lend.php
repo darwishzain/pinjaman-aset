a555f50745ff69490de133f025c57475ef1fa79f
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2026-05-10 16:59:48 +0800

    rework the flow. not much change except naming and remove. need to update T3.

---
+++ form/lend.php
@@ -0,0 +1,28 @@
+<?php
+include('config.php');
+include 'function.php';
+$content = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+
+}
+
+if($_GET)
+{
+
+}
+else
+{
+
+}
+//Status:
+//PENDING_MANAGER
+//REJECTED
+//PENDING_HANDLER
+//READY_FOR_HANDOVER
+//BORROWED
+//RETURNED
+//CANCELLED
+//OVERDUE
+?>
+<?php include('layout.php');?>
