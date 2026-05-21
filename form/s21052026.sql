9cdc56b6a87160ab3ac8e80742d691ababaaa44a
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2026-05-21 09:37:18 +0800

    Latest DB structure. Need to update ERD soon

--- form/s21052026.sql
+++ form/s21052026.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 12, 2026 at 10:17 AM
+-- Generation Time: May 21, 2026 at 03:30 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `bd_assetdb`
@@ -26,7 +32,8 @@ CREATE TABLE `T1_user` (
   `T1_username` varchar(100) NOT NULL,
   `T1_email` varchar(255) NOT NULL,
   `T1_passwordhash` varchar(255) NOT NULL,
-  `T1_role` enum('user','admin','pic') NOT NULL DEFAULT 'user'
+  `T1_type` varchar(50) NOT NULL DEFAULT 'user',
+  `T1_group` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -39,7 +46,7 @@ CREATE TABLE `T2_asset` (
   `T2_assetid` varchar(50) NOT NULL,
   `T2_label` varchar(255) DEFAULT NULL,
   `T2_type` varchar(20) NOT NULL,
-  `T2_handlerid` varchar(50) NOT NULL COMMENT 'userid of handler',
+  `T2T1_handlerid` varchar(50) NOT NULL COMMENT 'userid of handler',
   `T2_status` varchar(50) NOT NULL,
   `T2_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`T2_details`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -52,15 +59,17 @@ CREATE TABLE `T2_asset` (
 
 CREATE TABLE `T3_request` (
   `T3_requestid` varchar(50) NOT NULL,
-  `T3T1_userid` varchar(50) NOT NULL,
+  `T3T1_userid` varchar(50) NOT NULL COMMENT 'user that made request',
   `T3_submittime` datetime NOT NULL,
   `T3_reason` text NOT NULL,
-  `T3_remark` varchar(255) NOT NULL,
-  `T3_usage` varchar(30) NOT NULL,
-  `T3_type` varchar(30) NOT NULL,
-  `T3_approval` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`T3_approval`)),
+  `T3_remark` varchar(255) DEFAULT NULL,
+  `T3_purpose` varchar(30) NOT NULL,
+  `T3_type` enum('loan','book') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_nopad_ci NOT NULL COMMENT 'type loan or book',
+  `T3_datetouse` date NOT NULL,
+  `T3_managerapprove` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `T3_handlerapprove` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`T3_handlerapprove`)),
   `T3_status` varchar(40) NOT NULL,
-  `T3_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`T3_details`))
+  `T3_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -95,7 +104,7 @@ ALTER TABLE `T1_user`
 ALTER TABLE `T2_asset`
   ADD PRIMARY KEY (`T2_assetid`),
   ADD UNIQUE KEY `assetid` (`T2_assetid`),
-  ADD KEY `asset_handler` (`T2_handlerid`);
+  ADD KEY `asset_handler` (`T2T1_handlerid`);
 
 --
 -- Indexes for table `T3_request`
@@ -112,7 +121,7 @@ ALTER TABLE `T3_request`
 -- Constraints for table `T2_asset`
 --
 ALTER TABLE `T2_asset`
-  ADD CONSTRAINT `asset_handler` FOREIGN KEY (`T2_handlerid`) REFERENCES `T1_user` (`T1_userid`);
+  ADD CONSTRAINT `asset_handler` FOREIGN KEY (`T2T1_handlerid`) REFERENCES `T1_user` (`T1_userid`);
 
 --
 -- Constraints for table `T3_request`
@@ -120,3 +129,7 @@ ALTER TABLE `T2_asset`
 ALTER TABLE `T3_request`
   ADD CONSTRAINT `requestbyuser` FOREIGN KEY (`T3T1_userid`) REFERENCES `T1_user` (`T1_userid`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
