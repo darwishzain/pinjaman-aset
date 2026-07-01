8636f15a49097928c9623ed23c423a9c3b1e001a
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2026-07-01 13:51:34 +0800

    simple changes on page

--- laravel/resources/views/users/list.blade.php
+++ laravel/resources/views/users/list.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <a href="{{ route('user.add') }}" class="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded">
-                    Tambah Pengguna
+                    Daftar Pengguna
                 </a>
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h2 class="mb-4">Senarai Pengguna & Peranan</h2>
