88ccfd1225610cb0702824dcc941ff75eac9ff11
Darwish Zain @ Cendol <darwishzainstudio@gmail.com>
2026-07-09 13:04:39 +0800

    translate navigation

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -41,7 +41,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Profil') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -51,7 +51,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Log Keluar') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -74,7 +74,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Utama') }}
             </x-responsive-nav-link>
         </div>
 
@@ -87,7 +87,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Profil') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -97,7 +97,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Log Keluar') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
