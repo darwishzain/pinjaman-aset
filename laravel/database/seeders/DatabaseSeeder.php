da6dc40bfbfd115520dd72b608a8b3eea53514ca
Darwish Zain @ Cendol <darwishzainstudio@gmail.com>
2026-07-13 17:04:17 +0800

    Finish UI design for some module. user #5, asset #4, request(create request) #1. gitignore backup for drawio and docx.

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+        // ]);  
         $this->call([
             RoleAndUserSeeder::class,
         ]);
