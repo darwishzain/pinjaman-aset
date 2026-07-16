48fc38b63de07f8aaf8e6e94f5b2b11dc51b642e
Darwish Zain @ Cendol <darwishzainstudio@gmail.com>
2026-07-16 15:59:08 +0800

    add header for modal form made in livewire

---
+++ laravel/resources/views/components/modal-header.blade.php
@@ -0,0 +1,9 @@
+<div class="border-b p-6">
+    <h2 class="text-lg font-semibold">
+        {{ $slot }}
+
+        <button {{ $attributes }} class="float-right">
+            ✕
+        </button>
+    </h2>
+</div>
