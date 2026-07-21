1403d1882d9301767eafcc0e9ba20f2d11dbd1d3
Darwish Zain @ Cendol <darwishzainstudio@gmail.com>
2026-07-21 10:22:46 +0800

    update modal components and submit btn components

--- laravel/resources/views/components/modal-footer.blade.php
+++ laravel/resources/views/components/modal-footer.blade.php
@@ -1,9 +1,7 @@
-<div class="border-b p-6">
+<div class="flex justify-end gap-2 border-t p-6">
     <h2 class="text-lg font-semibold">
-        {{ $slot }}
-
         <button wire:click="$set('show', false)" class="float-right">
-            ✕
+            Batal
         </button>
     </h2>
 </div>
